6ffc5ce7ecadd91e8d6a09a7929597b72d8a1d90
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-10-13 12:29:00 +0600

    feat: tenancy/tenancy installed and configured

--- config/app.php
+++ config/app.php
@@ -185,6 +185,24 @@ return [
         /*
          * Package Service Providers...
          */
+        Tenancy\Affects\Broadcasts\Provider::class,
+        Tenancy\Affects\Cache\Provider::class,
+        Tenancy\Affects\Configs\Provider::class,
+        Tenancy\Affects\Connections\Provider::class,
+        Tenancy\Affects\Filesystems\Provider::class,
+        Tenancy\Affects\Logs\Provider::class,
+        Tenancy\Affects\Mails\Provider::class,
+        Tenancy\Affects\Models\Provider::class,
+        Tenancy\Affects\Routes\Provider::class,
+        Tenancy\Affects\URLs\Provider::class,
+        Tenancy\Affects\Views\Provider::class,
+
+        Tenancy\Hooks\Database\Provider::class,
+        Tenancy\Hooks\Migration\Provider::class,
+        Tenancy\Hooks\Hostname\Provider::class,
+
+        Tenancy\Database\Drivers\Mysql\Provider::class,
+        Tenancy\Database\Drivers\Sqlite\Provider::class,
 
         /*
          * Application Service Providers...
